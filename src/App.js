c935f565878d09102745823fd461e289ce5bb6b6
allconditionguy <asdesouza@st.knust.edu.gh>
2024-09-09 21:26:53 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -10,27 +10,23 @@ function App() {
     <Router>
   <div>
   <header className="bg-white text-black p-4">
-    <div className="container mx-auto flex flex-col items-center space-y-6">
- 
+    <div className="container mx-auto flex items-center justify-between flex-wrap">
       <div className="flex-shrink-0">
-        <h1 className="text-2xl font-bold text-gray-900">Dwaso</h1>
+        <h1 className="text-xl sm:text-2xl font-bold text-gray-900">Dwaso</h1>
       </div>
-
-    
-      <nav className="flex flex-col items-center space-y-4 font-poppins">
-        <a href="#about-us" className="hover:text-gray-500 transition-colors duration-200">
+       <nav className="flex space-x-4">
+        <a href="#about-us" className="hover:text-gray-500 transition-colors duration-200 text-sm sm:text-base">
           About Us
         </a>
-        <a href="#contact-us" className="hover:text-gray-500 transition-colors duration-200">
+        <a href="#contact-us" className="hover:text-gray-500 transition-colors duration-200 text-sm sm:text-base">
           Contact Us
         </a>
-        <a href="#more" className="hover:text-gray-500 transition-colors duration-200">
+        <a href="#more" className="hover:text-gray-500 transition-colors duration-200 text-sm sm:text-base">
           More
         </a>
       </nav>
-
-      <div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded font-poppins transition-transform duration-200 hover:scale-105">
+        <div className="flex-shrink-0">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded font-poppins text-sm sm:text-base transition-transform duration-200 hover:scale-105">
           Get Started
         </button>
       </div>
@@ -39,7 +35,7 @@ function App() {
 
   <div className="container mx-auto mt-8">
     <div>
-     
+      {/* Content */}
     </div>
   </div>
 </div>
